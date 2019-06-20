f92d821739ab0f5fdbee8067268373c702d5d834
DVitsme <derrick.valentine@gmail.com>
2019-06-20 04:48:29 -0400

    Used dangerouslySetInnerHTML / added styling / nested comments

--- components/Comment.js
+++ components/Comment.js
@@ -1,9 +1,52 @@
 const Comment = ({ comment }) => (
   <div className="comment">
     <div className="comment-user">{comment.user}</div>
-    <div className="comment-content">
-      <p>{comment.content}</p>
-    </div>
+    <div
+      className="comment-content"
+      dangerouslySetInnerHTML={{ __html: comment.content }}
+    />
+    {/* Some Comments have nested comments here we check for them and display */}
+    {comment.comments && (
+      <div className="nested-comments">
+        {comment.comments.map(nestedComment => (
+          // Nested Comments have the same structure as the first layer comment so that means I can reuse this same Comment component for them
+          <Comment key={nestedComment.id} comment={nestedComment} />
+        ))}
+      </div>
+    )}
+
+    {/* :global tag used to style the dangerouslySetInnerHTML */}
+    <style jsx>{`
+      .comment {
+        margin-bottom: 1.5em;
+      }
+      .comment-user {
+        font-size: 0.9rem;
+        font-weight: bold;
+        margin-bottom: 0.5em;
+      }
+      .comment-content {
+        font-size: 0.9rem;
+      }
+      .comment-content :global(p) {
+        margin: 0;
+        margin-bottom: 0.5em;
+        word-wrap: break-word;
+      }
+      .comment-content :global(a) {
+        color: #f60;
+        text-decoration: underline;
+      }
+      .comment-content :global(pre) {
+        max-width: 100%;
+        overflow: scroll;
+      }
+      .nested-comments {
+        margin-top: 1em;
+        border-left: 1px solid rgba(0, 0, 0, 0.1);
+        padding-left: 1em;
+      }
+    `}</style>
   </div>
 );
 
